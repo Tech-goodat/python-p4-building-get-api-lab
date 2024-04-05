3495fa05182cfcf3283ba3259b43271529cc0eb5
Tech-goodat <felixkiprotich2000@gmail.com>
2024-04-05 21:18:47 +0300

    bakeries lab

--- server/app.py
+++ server/app.py
@@ -20,15 +20,38 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries=[bakery.to_dict() for bakery in Bakery.query.all()]
+    response=make_response(
+        bakeries,
+        200
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery=Bakery.query.filter(Bakery.id==id).first()
+    bakery_dict=bakery.to_dict()
+    response=make_response(
+        bakery_dict,
+        200
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    
+   baked_good=Bakery.query.sorted(desc(Bakery.price)).first()
+   baked_good=[]
+   for baked_good in bakeries :
+    baked_good=bakeries.price
+    baked_good.append(bakeries)
+
+    response=make_response.jsonify(
+       baked_good,
+       200
+    )
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
